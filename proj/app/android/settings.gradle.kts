10d150498c55fa4cdffe64c8478c962605f663a1
myonlystarWang <54332782@qq.com>
2026-06-21 22:44:42 +0800

    chore: add gradle mirrors and root gitignore to fix build time hang

--- proj/app/android/settings.gradle.kts
+++ proj/app/android/settings.gradle.kts
@@ -11,6 +11,8 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
         google()
         mavenCentral()
         gradlePluginPortal()
